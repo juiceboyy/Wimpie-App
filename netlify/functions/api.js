7a7f987a51fcab431175fb1a016a5bf224f7f716
“Ro <halfhide@gmail.com>
2026-03-03 22:25:16 +0100

    return to official email address

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -203,9 +203,9 @@ async function saveReport(payload) {
 }
 
 async function sendExport(payload) {
-  // const toEmail = 'declaratieonderaannemers@cordaan.nl';
-  const toEmail = 'halfhide@gmail.com';
-  const ccEmail = ''; // TODO: Verander dit naar auckboersma@gmail.com voor live gaan
+  const toEmail = 'declaratieonderaannemers@cordaan.nl';
+  // const toEmail = 'halfhide@gmail.com';
+  const ccEmail = 'auckboersma@gmail.com'; // TODO: Verander dit naar auckboersma@gmail.com voor live gaan
   
   const maanden = ['januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli', 'augustus', 'september', 'oktober', 'november', 'december'];
   const [jaar, maandNummer] = payload.maand.split('-');
