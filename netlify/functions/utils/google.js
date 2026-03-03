5a2ba6791e3b2fde419ea542ffc4bb4b07fd7fa7
“Ro <halfhide@gmail.com>
2026-03-03 07:22:05 +0100

    fix google API key

--- netlify/functions/utils/google.js
+++ netlify/functions/utils/google.js
@@ -1,9 +1,15 @@
 const { google } = require('googleapis');
 
+let formattedPrivateKey = process.env.GOOGLE_PRIVATE_KEY || '';
+if (formattedPrivateKey.startsWith('"') && formattedPrivateKey.endsWith('"')) {
+  formattedPrivateKey = formattedPrivateKey.slice(1, -1);
+}
+formattedPrivateKey = formattedPrivateKey.replace(/\\n/g, '\n');
+
 const auth = new google.auth.GoogleAuth({
   credentials: {
     client_email: process.env.GOOGLE_CLIENT_EMAIL,
-    private_key: process.env.GOOGLE_PRIVATE_KEY ? process.env.GOOGLE_PRIVATE_KEY.replace(/\n/g, '\n') : undefined,
+    private_key: formattedPrivateKey,
   },
   scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
